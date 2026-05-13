f0200cb8f41770715969f9a2b62324bfb039104a
Jan Høydahl <jan.git@cominvent.com>
2026-05-13 19:59:37 +0200

    Update allowedUnsafeExecutions and allowedCommands

--- solr/renovate-config.js
+++ solr/renovate-config.js
@@ -12,7 +12,8 @@ module.exports = {
   // TODO: This is not working as expected. See https://github.com/renovatebot/renovate/issues/7850
   //baseBranches: ["main", "branch_9x"],
   //useBaseBranchConfig: "merge",
-  allowedCommands: ["./gradlew.*", "java -version.*install-tool java .*"],
+  allowedCommands: ["./gradlew.*", "java.*", "install-tool.*"],
+  allowedUnsafeExecutions: ["gradleWrapper"],
   dryRun: null,
   extends: [":semanticCommitsDisabled"],
 };
